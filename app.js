6192914376c2bdf4ccba8fbc19e26d5ded1fe1ad
ak-yas <lolesp295@gmail.com>
2022-11-04 19:25:27 +0000

    added resize & ShaderMaterial - scaffold complete.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 export default class Sketch {
   constructor(options) {
@@ -17,19 +18,46 @@ export default class Sketch {
     )
     this.camera.position.z = 1
 
+    this.control = new OrbitControls(this.camera, this.container)
+
     this.renderer = new THREE.WebGLRenderer({ antialias: true })
     this.renderer.setSize(this.width, this.height)
     this.container.appendChild(this.renderer.domElement)
 
+    this.resize()
+    this.setupResize()
     this.addObjects()
     this.render()
   }
 
-  resize() {}
+  resize() {
+    this.width = this.container.offsetWidth
+    this.height = this.container.offsetHeight
+    this.renderer.setSize(this.width, this.height)
+    this.camera.aspect = this.width / this.height
+    this.camera.updateProjectionMatrix()
+  }
+
+  setupResize() {
+    window.addEventListener('resize', this.resize.bind(this))
+  }
 
   addObjects() {
     this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
     this.material = new THREE.MeshNormalMaterial()
+
+    this.material = new THREE.ShaderMaterial({
+      flatShading: `
+       void main() {
+            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    `,
+      vertexShader: `
+        void main() {
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+        }
+      `,
+    })
 
     this.mesh = new THREE.Mesh(this.geometry, this.material)
     this.scene.add(this.mesh)
